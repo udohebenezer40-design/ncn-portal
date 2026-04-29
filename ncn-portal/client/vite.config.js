81c1bfdcad3313225226001df639ac0520b4bdac
Ebenezer <udohebenezer930@gmail.com>
2026-04-29 16:20:12 +0300

    fix: final vite config for root deployment

--- ncn-portal/client/vite.config.js
+++ ncn-portal/client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ncn-portal/',   // must match your GitHub repo name
+ base: '/',
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
